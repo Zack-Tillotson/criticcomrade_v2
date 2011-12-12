675d1544320cf8b84a5a4086f543004db36e2075
Zack Tillotson <ztillots@gmail.com>
2011-12-11 22:34:42 -0700

    Adding basic db layout

---
+++ production/db/db-init.sql
@@ -0,0 +1,31 @@
+create table cc_movie (
+	mid int primary key
+	,title char(255) not null
+	,imdb_id char(128)
+	,year char(4)
+	,url_title char(255) not null
+	,synopsis text
+	,runtime char(32)
+	,release_date datetime
+	,poster_link text
+	,mpaa_rating text
+);
+
+create table cc_critic (
+	cid int primary key auto_increment
+	,name text not null
+	,publication text not null
+);
+
+create table cc_review (
+	rid int primary key auto_increment
+	,is_positive bool not null
+	,link text
+	,original_score text
+	,date datetime not null
+	,mid int not null
+	,cid int not null
+	---------------------------
+	,foreign key(mid) references cc_movie(mid)
+	,foreign key(rid) references cc_critic(cid)
+);
