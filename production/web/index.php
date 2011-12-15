ef8d9e8dab820849e83f5c8ede0b09eda46449e1
Zack Tillotson <ztillots@gmail.com>
2011-12-15 10:47:55 -0700

    Starting with web page

---
+++ production/web/index.php
@@ -0,0 +1,33 @@
+<?php
+
+	// Iteration 1
+
+		// Goal
+			// 1. Get _something_ up and running
+			// 2. Don't spend a lot of time
+			// 3. Conversation seed
+
+		// Risks
+			// 1. Bite off too much
+			// 2. Not professional looking
+
+		// Plan
+			// 1. Start with most basic atomic functionality - rate movies and see what critics agree
+			// 2. Don't get fancy. Make basic now, will tweak look until professional.
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title>Critic Critic - Which Critic Agrees With You?</title>
+	</head>
+	<body>
+		<div id="section-title">
+			<h1>Critic Critic</h1>
+			<span>Which Critic Agrees With You?</span>
+		</div>
+		<div id="section-rate">
+		</div>
+		<div id="section-critics">
+		</div>
+	</body>
+</html>
